e821c0dfea171901177915c2887c095e33d811ed
Maitra Patel <60070105+pm161200@users.noreply.github.com>
2021-02-23 00:02:15 -0500

    E1 Complete

---
+++ Controllers/RideController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalRides.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalRides.Controllers
+{
+    public class RideController : Controller
+    {
+        public IActionResult Index()
+        {
+            var segments = new List<Segment>();
+            for (var i = 1; i <=10; i++)
+            {
+                segments.Add(new Segment { SegmentId = i, Name = "Segment " + i.ToString() });
+            }
+
+            return View(segments);
+        }
+
+        public IActionResult Browse(String segment)
+        {
+            ViewBag.segment = segment;
+            return View();
+        }
+
+        // /Store/AddCategory
+        public IActionResult AddSegment()
+        {
+            //load a form to capture a object from the user
+            return View();
+        }
+    }
+}
---
+++ Models/Segment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalRides.Models
+{
+    public class Segment
+    {
+        public int SegmentId { get; set; } //Pk
+        [Required]
+        public string Name { get; set; } 
+    }
+}
